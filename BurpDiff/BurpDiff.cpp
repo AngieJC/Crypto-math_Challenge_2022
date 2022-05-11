28ea6a626a98c26a7661e42c019e6acd7361d1aa
AngieJC <htk90uggk@outlook.com>
2022-05-11 16:13:57 +0800

    添加遍历差分代码

---
+++ BurpDiff/BurpDiff.cpp
@@ -0,0 +1,58 @@
+#include "CryptographicAlgorithm.h"
+#include <mpi.h>
+#include <iostream>
+#include <map>
+
+#define diffInL		0b0000000000000000
+#define diffInR		0b1000000000000000
+#define L0			0b0000000100100011
+#define R0			0b0100010101100111
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	/*if (argc == 1) {
+		cout << "需要运行在MPI环境下！" << endl;
+		return 0;
+	}
+	else if (argc != 2) {
+		cout << "运行方式：mpiexec -n [进程数] ./a.out [轮数]" << endl;
+		return 0;
+	}*/
+
+	// 读取需要遍历的轮数
+	int r = atoi(argv[1]);
+
+	// 初始化明文对和密钥
+	uint16_t p[2] = { L0, R0 }, p_[2] = {p[0] ^ diffInL, p[1] ^ diffInR}, c[2] = {0}, c_[2] = {0};
+	uint16_t seedkey[4] = { 0 };
+	uint64_t SeedKey = 0;
+
+	// 初始化MPI
+	int world_size, world_rank;
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	// 划分每个进程需要遍历的密钥空间
+	uint64_t v = pow(2, ((int)((r + 1) / 2 ) < 4 ? (int)((r + 1) / 2) : 4) * 16);
+	map<uint64_t, uint32_t> diffs; // <密钥, 输出差分>
+	for (SeedKey = world_rank * (v / world_size); SeedKey < (world_rank + 1) * (v / world_size); SeedKey++) {
+		memcpy((uint16_t*) & seedkey[0], ((uint16_t*) & SeedKey) + 0, 2);
+		memcpy((uint16_t*) & seedkey[1], ((uint16_t*) & SeedKey) + 1, 2);
+		memcpy((uint16_t*) & seedkey[2], ((uint16_t*) & SeedKey) + 2, 2);
+		memcpy((uint16_t*) & seedkey[3], ((uint16_t*) & SeedKey) + 3, 2);
+		// 加密
+		Enc(p, c, seedkey, r);
+		Enc(p_, c_, seedkey, r);
+		diffs[SeedKey] = pow(2, 16) * (c[0] ^ c_[0]) + (c[1] ^ c_[1]);
+	}
+
+	// 写入文本
+
+	// 进程同步
+	MPI_Barrier(MPI_COMM_WORLD);
+	cout << "进程" << world_rank << "退出" << endl;
+
+	return 0;
+}
