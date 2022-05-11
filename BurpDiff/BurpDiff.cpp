fa58f136e303191d8f1077a8f0f4551d81ef3c2f
AngieJC <htk90uggk@outlook.com>
2022-05-11 19:06:58 +0800

    不在内存中保存密钥和差分

--- BurpDiff/BurpDiff.cpp
+++ BurpDiff/BurpDiff.cpp
@@ -40,7 +40,12 @@ int main(int argc, char** argv) {
 
 	// 划分每个进程需要遍历的密钥空间
 	uint64_t v = pow(2, ((int)((r + 1) / 2 ) < 4 ? (int)((r + 1) / 2) : 4) * 16);
-	map<uint64_t, uint32_t> diffs; // <密钥, 输出差分>
+	// map<uint64_t, uint32_t> diffs; // <密钥, 输出差分>
+	char filename[32] = { 0 };
+	sprintf(filename, "r%d_th%d.csv", r, world_rank);
+	FILE* fp = fopen(filename, "w");
+	fprintf(fp, "k[3..0], 输出差分\n");
+	uint32_t diff = 0;
 	for (SeedKey = world_rank * (v / world_size); SeedKey < (world_rank + 1) * (v / world_size); SeedKey++) {
 		memcpy((uint16_t*) & seedkey[0], ((uint16_t*) & SeedKey) + 0, 2);
 		memcpy((uint16_t*) & seedkey[1], ((uint16_t*) & SeedKey) + 1, 2);
@@ -49,13 +54,16 @@ int main(int argc, char** argv) {
 		// 加密
 		Enc(p, c, seedkey, r);
 		Enc(p_, c_, seedkey, r);
-		diffs[SeedKey] = pow(2, 16) * (c[0] ^ c_[0]) + (c[1] ^ c_[1]);
+		// diffs[SeedKey] = pow(2, 16) * (c[0] ^ c_[0]) + (c[1] ^ c_[1]);
+		diff = pow(2, 16) * (c[0] ^ c_[0]) + (c[1] ^ c_[1]);
+		fprintf(fp, "%016X, %08X\n", SeedKey, diff);
 	}
+	fclose(fp);
 
 	// 写入文本
-	char filename[32] = { 0 };
+	/*char filename[32] = { 0 };
 	sprintf(filename, "r%d_th%d.csv", r, world_rank);
-	map2File(filename, &diffs);
+	map2File(filename, &diffs);*/
 
 	// 进程同步
 	MPI_Barrier(MPI_COMM_WORLD);
